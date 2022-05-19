873ed0c44b33e782c9998169e1b1359030fd1e94
larissadantaz <larissa.ribeiro@sptech.school>
2022-05-19 20:15:29 -0300

    tela de cadastro e login funcionando

--- site/app.js
+++ site/app.js
@@ -4,7 +4,7 @@ process.env.AMBIENTE_PROCESSO = "desenvolvimento";
 var express = require("express");
 var cors = require("cors");
 var path = require("path");
-var PORTA = 3333;
+var PORTA = 3330;
 
 var app = express();
 
