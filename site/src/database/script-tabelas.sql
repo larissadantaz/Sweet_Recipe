dabaa7db1af55514310ed0c686daa6265fffde98
larissadantaz <larissa.ribeiro@sptech.school>
2022-05-19 21:52:58 -0300

    Mural de Avisos e Dashboard funcionando

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -13,7 +13,13 @@ CREATE TABLE usuario (
 	email VARCHAR(50),
 	senha VARCHAR(50)
 );
-
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
 
 /* para sql server - remoto - produção */
 
