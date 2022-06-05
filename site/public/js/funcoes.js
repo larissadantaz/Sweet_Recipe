94c44d6b1df872af859403e134d47c24f01a67b0
larissadantaz <larissa.ribeiro@sptech.school>
2022-06-04 22:32:24 -0300

    Adicionando a opcao sair

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -24,7 +24,7 @@ function limparSessao() {
     // aguardar();
     sessionStorage.clear();
     // finalizarAguardar();
-    window.location = "login.html";
+    window.location = "../login.html";
 }
 
 // carregamento (loading)
